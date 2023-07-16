93acef06795f04fade860ed508361bb16959ef65
Gricha1 <trololoeeev@mail.ru>
2023-07-16 05:28:36 +0300

    gregory: fix safety target update

--- polamp_RIS.py
+++ polamp_RIS.py
@@ -398,6 +398,10 @@ class RIS(object):
 				target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 			for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
 				target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+			if self.safety:
+				for param, target_param in zip(self.critic_cost.parameters(), self.critic_target.parameters()):
+					target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
 
 		# debug
 		train_state = {"x_max": state[:, 0].max().item(),
