2202e4117dae49e1760e4f4e20962f3c67027737
Brian Angulo <brianangulo.yauri@gmail.com>
2023-11-08 22:22:21 +0300

    adding neccesary arguments

--- ris_validate_polamp_env.py
+++ ris_validate_polamp_env.py
@@ -21,6 +21,7 @@ from polamp_env.lib.utils_operations import generateDataSet
 
 if __name__ == "__main__":	
     parser = argparse.ArgumentParser()
+    # environment
     parser.add_argument("--env",                  default="polamp_env")
     parser.add_argument("--test_env",             default="polamp_env")
     parser.add_argument("--dataset",              default="cross_dataset_simplified") # medium_dataset, hard_dataset, ris_easy_dataset, hard_dataset_simplified
@@ -28,8 +29,7 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_curriculum_treshold", default=0.95, type=float) # medium dataset -> hard dataset
     parser.add_argument("--uniform_feasible_train_dataset", default=False)
     parser.add_argument("--random_train_dataset",           default=False)
-    parser.add_argument("--train_sac",            default=True, type=bool)
-    parser.add_argument("--validate_train_dataset",  default=True, type=bool)
+    parser.add_argument("--train_sac",            default=False, type=bool)
     # ris
     parser.add_argument("--epsilon",            default=1e-16, type=float)
     parser.add_argument("--n_critic",           default=2, type=int) # 1
@@ -41,31 +41,39 @@ if __name__ == "__main__":
     parser.add_argument("--n_eval",             default=5, type=int)
     parser.add_argument("--device",             default="cuda")
     parser.add_argument("--seed",               default=42, type=int) # 42
-    parser.add_argument("--exp_name",           default="polamp_env_sac_8")
+    parser.add_argument("--exp_name",           default="RIS_ant")
     parser.add_argument("--alpha",              default=0.1, type=float)
-    parser.add_argument("--Lambda",             default=10, type=float) # 0.1
-    parser.add_argument("--n_ensemble",         default=10, type=int) # 10
+    parser.add_argument("--Lambda",             default=0.1, type=float) # 0.1
+    parser.add_argument("--n_ensemble",         default=20, type=int) # 10
     parser.add_argument("--use_dubins_filter",  default=False, type=bool) # 10
     parser.add_argument("--h_lr",               default=1e-4, type=float)
     parser.add_argument("--q_lr",               default=1e-3, type=float)
     parser.add_argument("--pi_lr",              default=1e-4, type=float)
-    parser.add_argument("--clip_v_function",    default=-368, type=float) # -368
+    parser.add_argument("--clip_v_function",    default=-150, type=float) # -368
     parser.add_argument("--add_obs_noise",           default=False, type=bool)
     parser.add_argument("--curriculum_alpha_val",        default=0, type=float)
     parser.add_argument("--curriculum_alpha_treshold",   default=500000, type=int) # 500000
     parser.add_argument("--curriculum_alpha",        default=False, type=bool)
     parser.add_argument("--curriculum_high_policy",  default=False, type=bool)
+    parser.add_argument("--max_grad_norm",              default=6.0, type=float)
+    parser.add_argument("--scaling",              default=1.0, type=float)
+    parser.add_argument("--lambda_initialization",  default=0.1, type=float)
+    
+    # her
+    parser.add_argument("--fraction_goals_are_rollout_goals",  default=0.2, type=float) # 20
+    parser.add_argument("--fraction_resampled_goals_are_env_goals",  default=0.0, type=float) # 20
+    parser.add_argument("--fraction_resampled_goals_are_replay_buffer_goals",  default=0.5, type=float) # 20
     # encoder
     parser.add_argument("--use_decoder",             default=True, type=bool)
     parser.add_argument("--use_encoder",             default=True, type=bool)
-    parser.add_argument("--state_dim",               default=20, type=int) # 20
+    parser.add_argument("--state_dim",               default=40, type=int) # 20
     # safety
     parser.add_argument("--safety_add_to_high_policy", default=False, type=bool)
     parser.add_argument("--safety",                    default=False, type=bool)
-    parser.add_argument("--cost_limit",                default=0.5, type=float)
+    parser.add_argument("--cost_limit",                default=5.0, type=float)
     parser.add_argument("--update_lambda",             default=1000, type=int)
     # logging
-    parser.add_argument("--using_wandb",        default=True, type=bool)
+    parser.add_argument("--using_wandb",        default=False, type=bool)
     parser.add_argument("--wandb_project",      default="validate_ris_polamp", type=str)
     parser.add_argument('--log_loss', dest='log_loss', action='store_true')
     parser.add_argument('--no-log_loss', dest='log_loss', action='store_false')
@@ -97,8 +105,10 @@ if __name__ == "__main__":
         total_maps = 1
     dataSet = generateDataSet(our_env_config, name_folder=args.dataset, total_maps=total_maps, dynamic=False)
     maps, trainTask, valTasks = dataSet["obstacles"]
-    if args.validate_train_dataset:
-        valTasks = trainTask
+    print(f"trainTask: {len(trainTask['map0'])}")
+    print(f"valTasks: {len(valTasks['map0'])}")
+    # if args.validate_train_dataset:
+    #     valTasks = trainTask
     goal_our_env_config["dataset"] = args.dataset
     goal_our_env_config["uniform_feasible_train_dataset"] = args.uniform_feasible_train_dataset
     goal_our_env_config["random_train_dataset"] = args.random_train_dataset
@@ -109,7 +119,7 @@ if __name__ == "__main__":
     environment_config = {
         'vehicle_config': car_config,
         'tasks': trainTask,
-        'valTasks': valTasks,
+        'valTasks': trainTask,
         'maps': maps,
         'our_env_config' : our_env_config,
         'reward_config' : reward_config,
@@ -173,7 +183,7 @@ if __name__ == "__main__":
                  Lambda=args.Lambda, epsilon=args.epsilon,
                  h_lr=args.h_lr, q_lr=args.q_lr, pi_lr=args.pi_lr, 
                  n_ensemble=args.n_ensemble,
-                 clip_v_function=args.clip_v_function,
+                 clip_v_function=args.clip_v_function, max_grad_norm=args.max_grad_norm, lambda_initialization=args.lambda_initialization,
                  device=args.device, logger=logger if args.log_loss else None, 
                  env_obs_dim=env_obs_dim, add_ppo_reward=env.add_ppo_reward,
                  add_obs_noise=args.add_obs_noise,
@@ -210,41 +220,27 @@ if __name__ == "__main__":
     val_state, val_goal, \
     mean_actions, eval_episode_length, validation_info \
                     = evalPolicy(policy, test_env, 
-                                 plot_full_env=True,
-                                 plot_subgoals=False,
-                                 plot_value_function=False, 
-                                 render_env=False, 
-                                 plot_only_agent_values=False, 
-                                 plot_dubins_curve=False,
-                                 # medium dataset
-                                 #video_validate_tasks = [("map4", 8), ("map4", 13)],
-                                 #video_validate_tasks = [("map6", 5), ("map6", 18)],
-                                 #video_validate_tasks = [("map7", 19)],
-                                 #video_validate_tasks = [("map5", 7)],
-                                 #video_validate_tasks = [("map4", 8), ("map4", 13), ("map6", 5), ("map6", 18), ("map7", 19), ("map5", 7)],
-                                 # hard dataset
-                                 #video_validate_tasks = [("map0", 2), ("map0", 5), ("map0", 10), ("map0", 15)],
-                                 #video_validate_tasks = [("map0", 2)],
-                                 #video_validate_tasks = [("map0", 14), ("map0", 62), ("map0", 84), ("map0", 95), ("map0", 103), ("map0", 112), ("map0", 128), ("map0", 135)],
-                                 # hard dataset simplified
-                                 #video_validate_tasks = [("map0", 0), ("map0", 1), ("map0", 2)],
-                                 #video_validate_tasks = [("map0", 0), ("map0", 1), ("map0", 2), ("map0", 3)] if not args.validate_train_dataset else [("map0", i) for i in range(600)],
-                                 video_validate_tasks = [("map0", i) for i in range(600)],
-                                 #video_validate_tasks = [("map0", 1), ("map1", 1)],
-                                 value_function_angles=["theta_agent", 0, -np.pi/2],
-                                 plot_decoder_agent_states=False,
-                                 plot_subgoal_dispertion=True,
-                                 plot_lidar_predictor=False,
-                                 eval_strategy=None, # eval_strategy=action=[a, w], else None
-                                 skip_not_video_tasks=True,
-                                 plot_only_start_position=True)
+                                plot_full_env=True,
+                                plot_subgoals=True,
+                                plot_value_function=False,
+                                render_env=False,
+                                plot_only_agent_values=False, 
+                                plot_decoder_agent_states=False,
+                                plot_subgoal_dispertion=True,
+                                plot_lidar_predictor=False,
+                                #video_validate_tasks = [("map0", 0), ("map0", 1), ("map1", 0), ("map1", 1)],
+                                value_function_angles=["theta_agent", 0, -np.pi/2],
+                                dataset_plot=False,
+                                dataset_validation=args.dataset,
+                                full_validation=True)
     wandb_log_dict = {}
     for map_name, task_indx, video in validation_info["videos"]:
         wandb_log_dict["validation_video"+"_"+map_name+"_"+f"{task_indx}"] = wandb.Video(video, fps=10, format="gif")
     if args.using_wandb:
         run.log(wandb_log_dict)
     print("validation success rate:", success_rate)
-    print("action info:", validation_info["action_info"])
+    # print("action info:", validation_info["action_info"])
+    print("validation_info:", validation_info)
     print([task[1] for task in validation_info if task[2] == "success"])
 
     
